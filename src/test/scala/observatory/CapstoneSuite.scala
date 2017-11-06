89ca158c81872bc2ffb34cd920f5426e33ff99f2
Filip Srajer <filip.srajer@blindspot.ai>
2017-11-06 22:52:08 +0100

    Test raw data parsing

--- src/test/scala/observatory/CapstoneSuite.scala
+++ src/test/scala/observatory/CapstoneSuite.scala
@@ -1,6 +1,7 @@
 package observatory
 
 import org.junit.runner.RunWith
+import org.scalatest.BeforeAndAfterAll
 import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
@@ -11,4 +12,5 @@ class CapstoneSuite
     with ManipulationTest
     with Visualization2Test
     with Interaction2Test
+    with BeforeAndAfterAll
 
--- src/test/scala/observatory/ExtractionTest.scala
+++ src/test/scala/observatory/ExtractionTest.scala
@@ -1,10 +1,54 @@
 package observatory
 
+import java.time.LocalDate
+
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 
 trait ExtractionTest extends FunSuite {
 
-  
+  test("empty all") {
+    val year = 0
+    val stations = Extraction.spark.sparkContext.parallelize(List(""))
+    val temperatures = Extraction.spark.sparkContext.parallelize(List(""))
+
+    var computed = Extraction.locateTemperaturesSpark(year, stations, temperatures).collect()
+    val expected = Array[(LocalDate, Location, Temperature)]()
+
+    assert(computed.sameElements(expected))
+  }
+
+  test("empty stations") {
+    val year = 0
+    val stations = Extraction.spark.sparkContext.parallelize(List(""))
+    val temperatures = Extraction.spark.sparkContext.parallelize(List("4,,01,01,32"))
+
+    var computed = Extraction.locateTemperaturesSpark(year, stations, temperatures).collect()
+    val expected = Array[(LocalDate, Location, Temperature)]()
+
+    assert(computed.sameElements(expected))
+  }
+
+  test("empty temperatures") {
+    val year = 0
+    val stations = Extraction.spark.sparkContext.parallelize(List("4,,+1,+1"))
+    val temperatures = Extraction.spark.sparkContext.parallelize(List(""))
+
+    var computed = Extraction.locateTemperaturesSpark(year, stations, temperatures).collect()
+    val expected = Array[(LocalDate, Location, Temperature)]()
+
+    assert(computed.sameElements(expected))
+  }
+
+  test("both nonempty") {
+    val year = 0
+    val stations = Extraction.spark.sparkContext.parallelize(List("4,,+1,+2", "4,5,+1,+2"))
+    val temperatures = Extraction.spark.sparkContext.parallelize(List("4,,01,01,32", "4,10,01,01,32"))
+
+    var computed = Extraction.locateTemperaturesSpark(year, stations, temperatures).collect()
+    val expected = Array((LocalDate.of(year, 1, 1), Location(1, 2), 0))
+
+    assert(computed.sameElements(expected))
+  }
 }
