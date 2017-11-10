22d549c910ec0b45f3a259e86618df38994509e8
Filip Srajer <filip.srajer@blindspot.ai>
2017-11-10 14:30:39 +0100

    Implement temperatures interpolation skeleton

--- src/main/scala/observatory/Main.scala
+++ src/main/scala/observatory/Main.scala
@@ -7,6 +7,8 @@ object Main extends App {
   Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
 
   val temps = Extraction.locateTemperatures(1975, "/stations.csv", "/1975.csv")
+  val tempsAvg = Extraction.locationYearlyAverageRecords(temps)
+  val currTemp = Visualization.predictTemperature(tempsAvg, Location(0, 0))
 
   println(temps)
 
--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -1,11 +1,14 @@
 package observatory
 
 import com.sksamuel.scrimage.{Image, Pixel}
+import org.apache.spark.rdd.RDD
 
 /**
   * 2nd milestone: basic visualization
   */
 object Visualization {
+
+  val p_param = 2
 
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
@@ -13,7 +16,36 @@ object Visualization {
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
+    predictTemperatureSpark(spark.sparkContext.parallelize(temperatures.toSeq), location)
+  }
+
+  /**
+    * @param a First location
+    * @param b Second location
+    * @return Great-circle distance between a and b
+    */
+  def computeDist(a: Location, b: Location): Double = {
     ???
+  }
+
+  /**
+    * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
+    * @param location Location where to predict the temperature
+    * @return The predicted temperature at `location`
+    */
+  def predictTemperatureSpark(temperatures: RDD[(Location, Temperature)], location: Location): Temperature = {
+    val dists = temperatures.map(entry => (computeDist(entry._1, location), entry._2)).cache()
+
+    val min = dists.reduce((a, b) => if(a._1 < b._1) a else b)
+    if(min._1 < 1) {
+      // a close enough station (< 1km), take its temperature
+      min._2
+    }else{
+      // interpolate
+      val weights = dists.map(entry => (1 / Math.pow(entry._1, p_param), entry._2)).cache()
+      val sum = weights.keys.sum()
+      weights.map(entry => (entry._1 * entry._2) / sum).sum()
+    }
   }
 
   /**
