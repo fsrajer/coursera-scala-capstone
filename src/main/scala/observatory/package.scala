71922608bfcfc95491b807bcf1414700dda22a5f
Filip Srajer <filip.srajer@blindspot.ai>
2017-11-10 14:09:42 +0100

    Make the spark object visible in the whole package

--- src/main/scala/observatory/package.scala
+++ src/main/scala/observatory/package.scala
@@ -1,4 +1,13 @@
+import org.apache.spark.sql.SparkSession
+
 package object observatory {
   type Temperature = Double // °C, introduced in Week 1
   type Year = Int // Calendar year, introduced in Week 1
+
+  val spark: SparkSession =
+    SparkSession
+      .builder()
+      .appName("Observatory")
+      .config("spark.master", "local")
+      .getOrCreate()
 }
